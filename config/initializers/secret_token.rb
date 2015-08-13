627c851f0e5066246c7a7abd2183221baf016d5d
Brian Stolte <brian.stolte@gmail.com>
2015-08-13 04:08:09 +0000

    Initial commit for the FH quiz #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quister::Application.config.secret_key_base = '7b0cd913710e3285bcdd2a2f25f2f4d31fa57662949e59d6c988b6cb890e1078c8d33ca7c38619eb8974452be1b3f496ff4299224ef12ea90548c34fc41fb4d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quister::Application.config.session_store :cookie_store, key: '_quister_session'
